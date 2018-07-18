341c29229a8c016e9cc14afae289b1aeba8035a4
meijianwei <574116351@qq.com>
2018-07-19 00:46:56 +0800

    update

--- common/src/main/java/com/huawei/genex/common/domain/CommonTask.java
+++ common/src/main/java/com/huawei/genex/common/domain/CommonTask.java
@@ -4,14 +4,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class CommonTask {
     @Id
     @GeneratedValue
+    @Pattern(regexp = "^[0-9]{10}$")
+    @NotNull
     protected Long id;
     protected String commitBy;
     protected String description;
--- common/src/main/java/com/huawei/genex/common/handler/CommonExceptionHandler.java
+++ common/src/main/java/com/huawei/genex/common/handler/CommonExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.huawei.genex.common.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class CommonExceptionHandler {
     @ExceptionHandler
-    private void a(){
-
+    protected String exceptionHandle(Exception e){
+        log.info(e.getMessage(),e);
+        return e.getMessage();
     }
 }
